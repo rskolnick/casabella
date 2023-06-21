29eb35948ec4be59bc868a1ac9abb675006eadb5
Ryan Skolnick <rhskolnick@gmail.com>
2023-06-21 15:15:48 -0400

    added lang

--- app/(users)/layout.tsx
+++ app/(users)/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html>
+        <html lang="en">
             <body className="bg-gray-900">
                 <Header />
                 {children}
