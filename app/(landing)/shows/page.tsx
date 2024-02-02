94079cb260dcde44b6fcd21e99d611d22e8fd604
Ryan Skolnick <rhskolnick@gmail.com>
2024-02-02 14:55:21 -0500

    Changed the year

---
+++ app/(landing)/shows/page.tsx
@@ -0,0 +1,20 @@
+export default function ShowsPage() {
+    return (
+        <div>
+            <div className="bg-[url('/Venetian.jpg')] h-screen w-screen flex">
+                <div className="w-full h-full flex items-center justify-center">
+                    <div className="bg-gray-300 opacity-[.90] font-serif w-2/5 min-h-[11rem] text-emerald-900 flex flex-wrap items-center justify-center p-3 rounded-lg rounded-bl-none">
+                        <h1 className="text-4xl font-bold">
+                            Casa Bella Outdoor Kitchen Cabinetry
+                        </h1>
+                        <p className="text-2xl font-light">
+                            Learn how to increase your revenue by selling
+                            outdoor kitchens. Stop by our booth,{' '}
+                            <strong className="font-bold">Booth #1357</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-gray-200 border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                © 2023{' '}
+                © 2024{' '}
                 <Link href="#" className="hover:underline">
                     SDR Manufacturing, Inc
                 </Link>
